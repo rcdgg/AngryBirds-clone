607cd51a304b18cfc295b2ddc2254daf9a1415d8
shivank <shivank.rajput2005@gmail.com>
2024-10-25 19:46:59 +0530

    other pigs added

---
+++ core/src/main/java/birds/angry/GameSprites/SoldierPig.java
@@ -0,0 +1,10 @@
+package birds.angry.GameSprites;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+public class SoldierPig extends Pig{
+    public SoldierPig(Vector2 position) {
+        super(Assets.soldierpig, position, new Vector2((float)Assets.soldierpig.getWidth()-500, (float)Assets.soldierpig.getHeight()-500));
+    }
+}

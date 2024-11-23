46ee4c3db54e52d62cf36c31f275aab67e1f54b2
RohanS. <rohan23443@iiitd.ac.in>
2024-11-24 00:41:20 +0530

    birds added

--- core/src/main/java/birds/angry/Screens/Level1.java
+++ core/src/main/java/birds/angry/Screens/Level1.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -32,10 +33,10 @@ public class Level1 extends BaseScreen implements InputProcessor{
     private Stone stonelog;
     private World world;
     private Box2DDebugRenderer b2dr;
-    private Body redbirdBody;
-    private Body ground;
+    private Body redbirdBody, bluebirdBody, yellowbirdBody, ground;
     private OrthographicCamera camera;
     float PPM = 100.0f;
+    float bird_size = 0.3f;
     private BodyDef bodyDef;
     private FixtureDef fixtureDef;
     private Box2DDebugRenderer dbg;
@@ -55,10 +56,9 @@ public class Level1 extends BaseScreen implements InputProcessor{
         bodyDef = new BodyDef();
         fixtureDef = new FixtureDef();
 
-        bodyDef.position.set(1,10);
-        bodyDef.gravityScale = 1;
-        bodyDef.type = BodyDef.BodyType.DynamicBody;
-        redbirdBody = createBird();
+        redbirdBody = createBird(new Vector2(1, 10));
+        bluebirdBody = createBird(new Vector2(3, 10));
+        yellowbirdBody = createBird(new Vector2(2, 13));
 
 //        fixtureDef.isSensor = false;
 //        fixtureDef.restitution = 0.9f;
@@ -74,29 +74,31 @@ public class Level1 extends BaseScreen implements InputProcessor{
 
         bodyDef.position.set(1, 1);
         bodyDef.type = BodyDef.BodyType.StaticBody;
-        Body body = world.createBody(bodyDef);
+        Body ground = world.createBody(bodyDef);
 
 //        fixtureDef.isSensor = false;
 //        fixtureDef.restitution = 0.9f;
 //        fixtureDef.friction = 0.2f;
 //        fixtureDef.filter.maskBits = -1;
         PolygonShape p = new PolygonShape();
-        p.setAsBox(1,1);
+        p.setAsBox(16,1);
 
         fixtureDef.shape = p;
-        body.createFixture(fixtureDef);
+        fixtureDef.friction = 0.1f;
+        ground.createFixture(fixtureDef);
         p.dispose();
 
 //        Texture sling = new Texture(Gdx.files.internal("screens/levels/slingshot.png"));
         background = Assets.level1bg;
         slingshot = new Slingshot(new Vector2(6*grid_size, 4*grid_size));
         redbird = new Redbird(new Vector2(5,5));
-        redbird.setSize(2 * c.getRadius(), 2 * c.getRadius());
-        redbird.setOriginX(redbird.getWidth() / 2);
-        redbird.setOriginY(redbird.getHeight() /2);
+        redbird.setSize(2 * bird_size, 2 * bird_size);
+
 //        redbird.setPosition(new Vector2(4*grid_size, 4*grid_size));
         bluebird = new Bluebird(new Vector2(2.5f*grid_size, 4*grid_size));
+        bluebird.setSize(2.5f * bird_size, 2 * bird_size);
         yellowbird = new Yellowbird(new Vector2(1f*grid_size, 4*grid_size));
+        yellowbird.setSize(2 * bird_size, 2 * bird_size);
         woodlog = new Wood(new Vector2(11*grid_size, 4*grid_size));
         icelog = new Ice(new Vector2(14*grid_size, 4*grid_size));
         stonelog = new Stone(new Vector2(17*grid_size, 4*grid_size));
@@ -108,25 +110,15 @@ public class Level1 extends BaseScreen implements InputProcessor{
         pause = new TextButton("pause", skin);
         pause.setPosition(50, 50);
 //        pause.setTouchable(Touchable.enabled);
-//        stage.addActor(pause);
-//        stage.addActor(slingshot);
         uistage.addActor(pause);
-        stage.addActor(slingshot);
+//        stage.addActor(slingshot);
         stage.addActor(redbird);
-//        stage.addActor(bluebird);
-//        stage.addActor(yellowbird);
-//        stage.addActor(ppig);
-//        stage.addActor(kingPig);
-//        stage.addActor(soldierPig);
-        stage.addActor(woodlog);
-//        stage.addActor(icelog);
-//        stage.addActor(stonelog);
         stage.addActor(bluebird);
         stage.addActor(yellowbird);
 //        stage.addActor(ppig);
 //        stage.addActor(kingPig);
 //        stage.addActor(soldierPig);
-//        stage.addActor(woodlog);
+        stage.addActor(woodlog);
 //        stage.addActor(icelog);
 //        stage.addActor(stonelog);
         pause.addListener(new ClickListener() {
@@ -158,33 +150,57 @@ public class Level1 extends BaseScreen implements InputProcessor{
 
         redbird.setX(redbirdBody.getPosition().x - redbird.getWidth() / 2);
         redbird.setY(redbirdBody.getPosition().y - redbird.getHeight() / 2);
+
+        bluebird.setX(bluebirdBody.getPosition().x - bluebird.getWidth() / 2);
+        bluebird.setY(bluebirdBody.getPosition().y - bluebird.getHeight() / 2);
+
+        yellowbird.setX(yellowbirdBody.getPosition().x - yellowbird.getWidth() / 2);
+        yellowbird.setY(yellowbirdBody.getPosition().y - yellowbird.getHeight() / 2);
         stage.act(delta);
         stage.draw();
 //        uistage.act(delta);
 //        uistage.draw();
 //        update(delta);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.setColor(Color.GRAY);
+
+        float gridSpacing = grid_size;
+
+        for (float y = 0; y < stage.getHeight(); y += gridSpacing) {
+            shapeRenderer.line(0, y, stage.getWidth(), y);
+        }
+
+        for (float x = 0; x < stage.getWidth(); x += gridSpacing) {
+            shapeRenderer.line(x, 0, x, stage.getHeight());
+        }
+
+        shapeRenderer.end();
         world.step(1/60f, 6,2);
         dbg.render(world, stage.getCamera().combined);
         InputMultiplexer multiplexer = new InputMultiplexer();
         multiplexer.addProcessor(this);
         multiplexer.addProcessor(stage);
         Gdx.input.setInputProcessor(multiplexer);
-        b2dr.render(world, camera.combined);
+//        b2dr.render(world, camera.combined);
 //        batch.begin();
 //        batch.draw(Assets.redbirds[0].getTexture(), 100, 100);
 //        batch.end();
     }
+
+    public void grid(){
+
+    }
+
     public void update(float delta){
         world.step(1/60f, 6, 2);
     }
-    public Body createBird(){
-        Body bbody;
+    public Body createBird(Vector2 pos){
         BodyDef def = new BodyDef();
         def.type = BodyDef.BodyType.DynamicBody;
-        def.position.set(1,10);
+        def.position.set(pos);
         Body bbody = world.createBody(def);
         CircleShape circle = new CircleShape();
-        circle.setRadius(0.5f);
+        circle.setRadius(bird_size);
         FixtureDef fdef = new FixtureDef();
         fdef.shape = circle;
         fdef.restitution = 0.5f;

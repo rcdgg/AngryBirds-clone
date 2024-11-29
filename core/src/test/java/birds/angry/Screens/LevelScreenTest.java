33c1406759bd016de91c3054b6e391799776124a
RohanS. <rohan23443@iiitd.ac.in>
2024-11-29 17:12:25 +0530

    junit working now

--- core/src/test/java/birds/angry/Screens/LevelScreenTest.java
+++ core/src/test/java/birds/angry/Screens/LevelScreenTest.java
@@ -2,31 +2,32 @@ package birds.angry.Screens;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class LevelScreenTest {
+public class LevelScreenTest {
 
     @org.junit.jupiter.api.Test
-    void save_game() {
+    public void save_game() {
         System.out.println("ok");
     }
 
     @org.junit.jupiter.api.Test
-    void load_game() {
+    public void load_game() {
 
         System.out.println("ok");
+        fail();
     }
 
     @org.junit.jupiter.api.Test
-    void win_condition() {
+    public void win_condition() {
         int score = 1;
         int levelMAXscore = 100;
         boolean won = false;
         if(score>levelMAXscore){
             won = true;
-//            System.out.println("You win!");
+            System.out.println("You win!");
         }
         else{
             won = false;
-//            System.out.println("You lose!");
+            System.out.println("You lose!");
         }
         assertFalse(won);
     }
